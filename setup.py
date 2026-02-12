22174ec747076e71a78fd86ed6bec287bdba34d9
dcuevasa <d.cuevas@uniandes.edu.co>
2026-02-12 17:40:11 +0000

    working code

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'talker = lab_1_ros.talker:main',
+            'listener = lab_1_ros.listener:main',
         ],
     },
 )
